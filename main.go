ef56f127c4f8e1d6adc804cc307275f0808619f1
karnerfly <toufique26ajay@gmail.com>
2025-07-31 18:27:40 +0530

    improve calculation and display file size in frontend

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/karnerfly/fileupload/files"
 	"github.com/karnerfly/fileupload/handlers"
@@ -12,37 +11,27 @@ import (
 )
 
 func main() {
-
 	addr := flag.String("addr", "3000", "server listen address.")
-	baseDir := flag.String("dir", "./data", "file storage base directory path.")
+	baseDir := flag.String("dir", "uploads", "file storage base directory path.")
 	maxSize := flag.Int64("filelimit", 5, "maximum file size that can be uploaded by server in mb.")
 	flag.Parse()
 
 	mux := http.NewServeMux()
 
-	store := files.NewLocalStorage(*baseDir, 1024*1024*(*maxSize))
+	store := files.NewLocalStorage(*baseDir, (*maxSize)<<20)
 
 	fh := handlers.NewFileHandler(store)
 	fileserver := http.FileServer(http.Dir(*baseDir))
 
-	// REST Apis
-	mux.Handle("POST /files/{id}/{filename}", middlewares.ValidatePath(http.HandlerFunc(fh.UploadREST)))
+	// default handler to get files
 	mux.Handle("GET /files/{id}/{filename}", middlewares.ValidatePath(http.StripPrefix("/files/", fileserver)))
+
+	// REST Apis
+	mux.Handle("POST /api/files/{id}/{filename}", middlewares.ValidatePath(http.HandlerFunc(fh.UploadREST)))
 
 	// Multipart form routes
 	mux.Handle("GET /upload", http.HandlerFunc(fh.ShowFormPage))
 	mux.Handle("POST /files/upload", middlewares.ValidateMultipartForm(http.HandlerFunc(fh.UplaodMultipart)))
-
-	// test the gzip middleware
-	mux.Handle("GET /test", middlewares.GzipMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		data, err := os.ReadFile("./demo.txt")
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		w.Write(data)
-	})))
 
 	server := http.Server{
 		Addr:    ":" + *addr,
